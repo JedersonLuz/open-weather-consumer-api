d77758c55e4a3f7323179dfad8fc77e9535966d3
Jederson Sousa Luz <jedersonalpha@gmail.com>
2023-01-11 09:08:54 -0300

    Update main.py
    POST route completely implemented to receive request create background tasks to execute the process asynchronously storing the acquired data in the database

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ from dotenv import load_dotenv
 from fastapi import FastAPI, BackgroundTasks
 from pydantic import BaseModel
 
+from utils.database_connector import DatabaseConnector
 
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
 
+with open('cities_id_list.txt', 'r') as f:
+    CITIES_ID_LIST = f.read().splitlines()
+    TOTAL_CITIES = len(CITIES_ID_LIST)
+
+db = DatabaseConnector()
 
 class User(BaseModel):
     user_id: str
@@ -18,11 +24,30 @@ class User(BaseModel):
 app = FastAPI()
 
 
-def get_weather_data(user_id: str) -> dict:
-    url = f"https://api.openweathermap.org/data/2.5/weather?id=3439525&appid={API_KEY}&units=metric"
-    response = requests.get(url)
-    print(response.json())
-    return response.json()
+def get_weather_data(user_id: str):
+    cities_weather_data = {}
+    cities_weather_data['cities'] = []
+    try:
+        db.create_weather_request(user_id, TOTAL_CITIES)
+        for num_cities, city_id in enumerate(CITIES_ID_LIST, 1):
+            citie_weather_data = {}
+            citie_weather_data['city_id'] = city_id
+            
+            url = f"https://api.openweathermap.org/data/2.5/weather?id={city_id}&appid={API_KEY}&units=metric"
+            response = requests.get(url)
+            
+            temprature = response.json()['main']['temp']
+            humidity = response.json()['main']['humidity']
+            
+            citie_weather_data['temprature'] = temprature
+            citie_weather_data['humidity'] = humidity
+            cities_weather_data['cities'].append(citie_weather_data)
+
+            db.update_weather_request(user_id, cities_weather_data, num_cities)
+        print(f'Weather data for user {user_id} was successfully collected')
+    except Exception as e:
+        print(f'Weather data for user {user_id} was not completely collected')
+        print(e)
 
 
 @app.get("/")
